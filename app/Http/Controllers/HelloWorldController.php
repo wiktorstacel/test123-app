5de9ea59998e6d056b84359eef54a56cd4ed28b8
wiktorstacel <stawik964@gmail.com>
2024-12-18 19:26:47 +0100

    Routing do zasobu - funkcja show w HelloWorldController

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    public function show()
+    {
+        return view('hello');
+    }
+}
