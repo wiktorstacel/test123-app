27551e9ba05fe175864ed65ea892dee77fad6694
wiktorstacel <stawik964@gmail.com>
2025-01-10 17:24:12 +0100

    Install registration and login system: github.com/laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', [HelloWorldController::class, 'show']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
